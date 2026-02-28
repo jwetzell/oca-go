8dade94f9a93828b60f27ddade3a2bf030fa0aae
Joel Wetzell <git@jwetzell.com>
2026-02-27 22:34:51 -0600

    add const for standard object numbers

--- oca.go
+++ oca.go
@@ -12,3 +12,18 @@ const (
 	OcaNotificationDeliveryModeNormal      OcaNotificationDeliveryMode = 1
 	OcaNotificationDeliveryModeLightweight OcaNotificationDeliveryMode = 2
 )
+
+const (
+	OcaDeviceManager          OcaONo = 1
+	OcaSecurityManager        OcaONo = 2
+	OcaFirmwareManager        OcaONo = 3
+	OcaSubscriptionManager    OcaONo = 4
+	OcaPowerManager           OcaONo = 5
+	OcaNetworkManager         OcaONo = 6
+	OcaMediaClockManager      OcaONo = 7
+	OcaAudioProcessingManager OcaONo = 9
+	OcaDeviceTimeManager      OcaONo = 10
+	OcaDiagnosticManager      OcaONo = 13
+	OcaLockManager            OcaONo = 14
+	OcaBlockManager           OcaONo = 100
+)
