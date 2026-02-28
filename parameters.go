c1a966c2978e0911819218bbc7dd6e5ad4eb7b25
Joel Wetzell <git@jwetzell.com>
2026-02-28 15:32:11 -0600

    add parameter for set enabled response

--- parameters.go
+++ parameters.go
@@ -16,6 +16,16 @@ type Ocp1Parameters struct {
 	Parameters     []Ocp1Parameter
 }
 
+func NewParameters(parameters []Ocp1Parameter) (Ocp1Parameters, error) {
+	if len(parameters) > 255 {
+		return Ocp1Parameters{}, fmt.Errorf("too many parameters: %d", len(parameters))
+	}
+	return Ocp1Parameters{
+		ParameterCount: uint8(len(parameters)),
+		Parameters:     parameters,
+	}, nil
+}
+
 func (p *Ocp1Parameters) String() string {
 	return fmt.Sprintf("%+v", p.Parameters)
 }
@@ -50,9 +60,23 @@ type DefLevelDecoders map[uint16]ParameterDecoders
 type ObjectDecoders map[OcaONo]DefLevelDecoders
 
 var OcaObjectDecoders = ObjectDecoders{
+	OcaDeviceManager: {
+		3: {
+			12: []ParameterDecoder{ // SetEnabled(OcaBool)
+				func(data []byte) (Ocp1Parameter, uint16, error) {
+					ocaBool := OcaBool(true)
+					err := ocaBool.UnmarshalBinary(data)
+					if err != nil {
+						return nil, 0, err
+					}
+					return ocaBool, 1, nil
+				},
+			},
+		},
+	},
 	OcaSubscriptionManager: {
 		3: {
-			8: []ParameterDecoder{
+			8: []ParameterDecoder{ // AddSubscription2(OcaEvent, OcaNotificationDeliveryMode, OcaBlob)
 				func(data []byte) (Ocp1Parameter, uint16, error) {
 					ocaEvent := OcaEvent{}
 					err := ocaEvent.UnmarshalBinary(data)
@@ -81,17 +105,17 @@ var OcaObjectDecoders = ObjectDecoders{
 func (od ObjectDecoders) GetParameterDecoders(targetONo OcaONo, defLevel uint16, methodIndex uint16) ([]ParameterDecoder, error) {
 	objectDecoder, ok := od[targetONo]
 	if !ok {
-		return nil, fmt.Errorf("Ocp1Command: no decoder found for TargetONo %d", targetONo)
+		return nil, fmt.Errorf("Ocp1Command: no decoder found for TargetONo %d.%d.%d", targetONo, defLevel, methodIndex)
 	}
 
 	defLevelDecoder, ok := objectDecoder[defLevel]
 	if !ok {
-		return nil, fmt.Errorf("Ocp1Command: no decoder found for DefLevel %d", defLevel)
+		return nil, fmt.Errorf("Ocp1Command: no decoder found for DefLevel %d.%d.%d", targetONo, defLevel, methodIndex)
 	}
 
 	parameterDecoders, ok := defLevelDecoder[methodIndex]
 	if !ok {
-		return nil, fmt.Errorf("Ocp1Command: no decoder found for MethodIndex %d", methodIndex)
+		return nil, fmt.Errorf("Ocp1Command: no decoder found for MethodIndex %d.%d.%d", targetONo, defLevel, methodIndex)
 	}
 
 	return parameterDecoders, nil
