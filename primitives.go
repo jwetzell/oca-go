0bfc9fc8f781f471dcf56472e77c899af0393742
Joel Wetzell <git@jwetzell.com>
2026-02-27 21:51:19 -0600

    blob length is a uint16

--- primitives.go
+++ primitives.go
@@ -1,6 +1,8 @@
 package oca
 
-import "errors"
+import (
+	"errors"
+)
 
 type OcaBlob []byte
 
@@ -8,21 +10,22 @@ func (b *OcaBlob) UnmarshalBinary(data []byte) error {
 	if len(data) < 1 {
 		return errors.New("OcaBlob: not enough data")
 	}
-	length := data[0]
-	if len(data) < int(1+length) {
+	length := uint16(data[0])<<8 | uint16(data[1])
+	if len(data) < int(2+length) {
 		return errors.New("OcaBlob: not enough data for blob content")
 	}
-	*b = data[1 : 1+length]
+	*b = data[2 : 2+length]
 	return nil
 }
 
 func (b *OcaBlob) MarshalBinary() ([]byte, error) {
 	length := len(*b)
-	if length > 255 {
-		return nil, errors.New("OcaBlob: blob too large")
+	if length > 65535 {
+		return nil, errors.New("OcaBlob: blob too large to marshal")
 	}
-	data := make([]byte, 1+length)
-	data[0] = byte(length)
+	data := make([]byte, 2+length)
+	data[0] = byte(length >> 8)
+	data[1] = byte(length & 0xff)
 	data = append(data, *b...)
 	return data, nil
 }
